6adcfbc0235b9cff4f16cc14c48200c976e87cab
ziembaej <ziembaej@gmail.com>
2021-11-16 19:54:00 -0500

    cleaned up notes for coop data cleaning

--- Local Cleaning/Compile Coop Data.R
+++ Local Cleaning/Compile Coop Data.R
@@ -3,31 +3,15 @@ library(openxlsx)
 library(readxl)
 library(tidyverse)
 library(rlist)
-# path = "/Users/eric.ziemba/Downloads/Wego Bill Verification Vol. III.xlsx"
-# listsheets = list(excel_sheets(path))
-# 
-# dt = read_excel(path, sheet = "Instructions")
-# 
-# df = data.frame()
-# setDT(df)
-# df[, `:=`(Link = "",
-#           Invoice_Date = as.Date(1),
-#           Created_at = as.Date(1),	
-#           Amount_Due = as.numeric(1),
-#           "# Lines" = as.numeric(0),
-#           Balance_Forward = as.character(x),
-#           Negative_Line_Item	Summary	ECM	Importer	Need to Check	Checked	Reason for Edit	If BF Removed, Why?	
-#             Notes = as.character(a)]
-
+# set the working directory
 path <- "/Users/eric.ziemba/Downloads/"
-# set the working directory 
 setwd(path)
 
-# accessing all the sheets 
+# accessing all the sheets
 sheets = excel_sheets("Wego Bill Verification Vol. IV.xlsx")
 d <- list()
 for(i in sheets){
-  a = read_excel("Wego Bill Verification Vol. IV.xlsx", sheet=i, col_names = TRUE, 
+  a = read_excel("Wego Bill Verification Vol. IV.xlsx", sheet=i, col_names = TRUE,
                  col_types = c("text", "date", "guess", "guess", "guess",
                                "guess", "guess", "guess", "guess", "guess",
                                "guess", "guess", "guess", "guess", "guess"))
@@ -40,4 +24,3 @@ datr <- new[, c("Link", "Balance Forward", "Importer", "Reason for Edit", "If BF
 setnames(datr, old = c("Link", "Balance Forward", "Importer", "Reason for Edit", "If BF Removed, Why?", "Notes"),
          new = c("Link", "BF", "Importer", "ReasonEdit", "BFWhy", "Notes"))
 datr = datr[!is.na(BF),]
-
