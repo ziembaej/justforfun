bf5fae73084d9c321f0fc31cc4510d78c8103de9
ziembaej <ziembaej@gmail.com>
2021-12-01 13:00:28 -0500

    missing commit

--- Local Cleaning/Compile Coop Data.R
+++ Local Cleaning/Compile Coop Data.R
@@ -3,6 +3,7 @@ library(openxlsx)
 library(readxl)
 library(tidyverse)
 library(rlist)
+library(lubridate)
 
 path <- "/Users/eric.ziemba/Downloads/"
 # set the working directory
@@ -21,7 +22,25 @@ for(i in sheets){
 
 new = rbindlist(d, fill = TRUE, use.names = TRUE, idcol = "ID")
 setDT(new)
-datr <- new[, c("Link", "Balance Forward", "Importer", "Reason for Edit", "If BF Removed, Why?", "Notes")]
-setnames(datr, old = c("Link", "Balance Forward", "Importer", "Reason for Edit", "If BF Removed, Why?", "Notes"),
-         new = c("Link", "BF", "Importer", "ReasonEdit", "BFWhy", "Notes"))
+datr <- new[, c("Link", "Created at", "Balance Forward", "Importer", "Reason for Edit", "If BF Removed, Why?", "Notes")]
+setnames(datr, old = c("Link",  "Created at", "Balance Forward", "Importer", "Reason for Edit", "If BF Removed, Why?", "Notes"),
+         new = c("Link",  "bill_created","BF", "Importer", "ReasonEdit", "BFWhy", "Notes"))
 datr = datr[!is.na(BF),]
+
+datr[,  bill_created_month := floor_date(as.Date(bill_created, format ="YYYY-MM-DD"), unit = "month")]
+
+
+
+datr[, ct :=1]
+imps <- datr[, .(imp_ct = sum(ct, na.rm=T)) , keyby = Importer]
+imp2 <- imps[imp_ct>50,]
+
+reasons <- datr[!is.na(ReasonEdit), .(count = sum(ct, na.rm=T)), keyby = .(Importer, BFWhy, Notes) ]
+
+late <- datr[str_detect(BFWhy, regex("late", ignore_case = TRUE)), 
+             .(count = sum(ct, na.rm=T)), keyby = .(bill_created_month, Importer, BFWhy, Notes) ]
+
+late_w_links <- datr[str_detect(BFWhy, regex("late", ignore_case = TRUE)),] 
+
+  
+  
